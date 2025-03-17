b2c6943b09faf2de0c8bc6760a2f83dd68068f4d
Vanderlei-Ny <vanderleijunior634@gmail.com>
2025-03-17 15:03:59 -0300

    Testando

---
+++ src/main/java/dev/instituicao/CadastroAlunosCursos/CadastroAlunosCursosApplication.java
@@ -0,0 +1,13 @@
+package dev.instituicao.CadastroAlunosCursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroAlunosCursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroAlunosCursosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/instituicao/CadastroAlunosCursos/CadastroAlunosCursosApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.instituicao.CadastroAlunosCursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroAlunosCursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
