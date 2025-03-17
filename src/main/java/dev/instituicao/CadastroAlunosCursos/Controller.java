2761870f63248c2be4fd64f43077c611b9591062
Vanderlei-Ny <vanderleijunior634@gmail.com>
2025-03-17 20:30:55 -0300

    Create the controller

---
+++ src/main/java/dev/instituicao/CadastroAlunosCursos/Controller.java
@@ -0,0 +1,28 @@
+package dev.instituicao.CadastroAlunosCursos;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping //rotas para um determinado destino
+@RestController //diz ao sistema que esse é o controlador
+public class Controller {
+
+	int Calculando;
+	
+	@GetMapping("/abrobinha") //Puxa as informações
+	public String boasVindas() {
+		return "Hello SpringBoot!";
+	}
+
+	@GetMapping("Cagando")
+	public int Calculando(int Calculando) {
+		this.Calculando = 12+2321+12;
+		return Calculando;
+	}
+	
+	
+	
+	
+
+}
